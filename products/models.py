8c464e9fef77ea9d57413f9bf7bf481ad18e99b5
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-02-14 13:49:04 +0000

    add label for sizing.html

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-       
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -29,3 +29,5 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
